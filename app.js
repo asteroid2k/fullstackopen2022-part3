d0e7575b16478688eab02b8c4e46ec46bc234e62
Kwaku Adusei Okyere <32445365+asteroid2k@users.noreply.github.com>
2022-03-28 13:07:58 +0000

    part3a

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+morgan.token("data", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+const logger = morgan(
+  "method :url :status :res[content-length] - :response-time ms :data"
+);
+app.use(logger);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/info", (req, res) => {
+  const peopleCount = persons.length + 1;
+  const now = new Date();
+  res.send(
+    `Phonebook has info for ${peopleCount} person(s)\n ${now.toString()}`
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+  const validate = name && number && name.length && number.toString().length;
+  if (!validate) {
+    return res.status(400).json({ error: "Name and number required" });
+  }
+
+  const exists = persons.findIndex(
+    (p) => p.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+  );
+  if (exists > -1) {
+    return res.status(400).json({ error: "Name exists already" });
+  }
+  const person = {
+    id: Number(Math.random().toString().substring(6)),
+    name,
+    number,
+  };
+  persons.push(person);
+  return res.status(201).json(person);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    return res.status(404).send("Person Not Found");
+  }
+  return res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    return res.status(404).send("Person Not Found");
+  }
+  persons = persons.filter((p) => p.id !== id);
+  return res.sendStatus(204);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
